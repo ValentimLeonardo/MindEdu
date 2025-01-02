2d3d44102237e00809b47fde8dd4398f5c029208
Leonardo <leonardo.valentim.dos.santos.2@gmail.com>
2025-01-02 19:59:24 -0300

    cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./pages/Login/login";
 import HomePage from "./pages/Homepage/homePage"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cadastro from "./pages/Cadastro/cadastro";
 
 function App() {
   return(
@@ -11,6 +12,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Login/>}/>
             <Route path="/homePage" element={<HomePage/>}/>
+            <Route path='/cadastro' element={<Cadastro/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Cadastro/cadastro.jsx
@@ -0,0 +1,45 @@
+import "./cadastro.css"
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Cadastro = () =>{
+    return(
+        <div className="container d-flex flex-column text-center align-items-center mt-5 position-absolute top-50 start-50 translate-middle bg-dark text-white p-3">
+            <h1>Cadastro</h1>
+               <form action="." method="post">
+                    <div className="row mb-3">
+                        <div className=" col-12 col-md-6">
+                            <label htmlFor="nome">Nome Completo</label>
+                            <input type="text" id="nome" name="nome" className="form-control form-control-lg"/>
+                        </div>
+                        
+                        <div className="col-12 col-md-6">
+                            <label htmlFor="email">E-mail</label>
+                            <input type="email" id="email" name="email" className="form-control form-control-lg"/>
+                        </div>
+                    </div>
+                    
+                    <div className="row mb-3">
+                        <div className=" col-12 col-md-6">
+                            <label htmlFor="senha">Senha</label>
+                            <input type="password" id="senha" name="senha" className="form-control form-control-lg"/>
+                        </div>
+                        
+                        <div className="col-12 col-md-6">
+                            <label htmlFor="nascimento">Data de nascimento:</label>
+                            <input type="date" id="nascimento" name="nascimento" className="form-control form-control-lg"/>
+                        </div>
+                    </div>
+
+                    <label htmlFor="ano">Série/Ano</label>
+                    <select id="ano" name="ano">
+                        <option value="1">1º ano</option>
+                        <option value="2">2º ano</option>
+                        <option value="3">3º ano</option> 
+                    </select>
+               </form>       
+        </div>
+    )
+}
+
+export default Cadastro;
